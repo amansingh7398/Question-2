01659c009c9814fa3b75a01316b54f47b1060845
amansingh7398 <kingaman1509@gmail.com>
2022-12-05 13:09:37 +0530

    Okie

---
+++ index.js
@@ -0,0 +1,9 @@
+/*function getData() {
+    fetch('https://raw.githubusercontent.com/debojyoti/places-fake-rest-api/master/cafes.json')
+        .then(response => {
+            return response.json();
+        }).then(data => {
+            console.log(data, "response data");
+        })
+
+}*/
